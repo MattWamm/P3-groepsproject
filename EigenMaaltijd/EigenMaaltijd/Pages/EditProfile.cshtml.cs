c23b16ef3dec8609d751bd88285a63d75fdceb89
michael <michael-klaassen@hotmail.com>
2021-03-30 01:56:38 +0200

    update edit page

--- EigenMaaltijd/EigenMaaltijd/Pages/EditProfile.cshtml.cs
+++ EigenMaaltijd/EigenMaaltijd/Pages/EditProfile.cshtml.cs
@@ -2,15 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApplication1.Pages
+namespace EigenMaaltijd.Pages
 {
     public class EditProfileModel : PageModel
     {
+        [BindProperty]
+        public User UserData
+        {
+            get
+            {
+                string cookie = Request.Cookies["keepLogin"];
+                return new UserRepository().getUserFromID(Convert.ToInt32(cookie));
+            }
+        }
+        [BindProperty]
+        public LoginUser LoginData
+        {
+            get
+            {
+                string cookie = Request.Cookies["keepLogin"];
+                return new UserRepository().getLoginUserFromID(Convert.ToInt32(cookie));
+            }
+        }
+
+        [BindProperty]
+        public RegisterLUser RegisterData { get; set; }
+
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnGetLogout()
+        {
+            Response.Cookies.Delete("keepLogin");
+            return RedirectToPage("Index");
         }
     }
 }
